f52bd63b73642000c1968bfa13ce0e2e41ef6d9a
Andreas Nikkolas Saputra <160854949+CamzieAmepzo@users.noreply.github.com>
2024-11-15 17:44:38 +0700

    routing part 2

--- belajar_angular/src/app/housing-location.ts
+++ belajar_angular/src/app/housing-location.ts
@@ -1,11 +1,10 @@
 export interface HousingLocation {
-    //Interface bisa menggambarkan kolom" dari collection (model dari monggoDB)
     id: number,
-    name : string,
+    name: string,
     city: string,
     state: string,
     photo: string,
-    availableUnit: number,
+    availableUnits: number,
     wifi: boolean,
     laundry: boolean
 }
--- my-pariwisata-web/routes/index.js
+++ my-pariwisata-web/routes/index.js
@@ -3,17 +3,19 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Beranda' });
+  res.render('index', { title: 'Halaman Beranda' });
 });
 
 router.get('/paketwisata', function(req, res, next) {
   let listpaketwisata = [
-    {'nama' : 'Wisata Pulau Kemaro', 'harga' : 50000},
+    {'nama' : 'Wisata Pulau Kemaro', 'harga' : 500000},
     {'nama' : 'Wisata Punti Kayu', 'harga' : 250000},
-    {'nama' : 'Wisata Jakabaring', 'harga' : 100000}
+    {'nama' : 'Wisata Jakabaring', 'harga' : 100000},
   ]
-  res.render('paketwisata', { title: 'Pakes Wisata',
-    listpaketwisata : listpaketwisata });
+  res.render('paketwisata', { 
+    title: 'Paket Wisata', 
+    listpaketwisata: listpaketwisata 
+  });
 });
 
 router.get('/orderpaket', function(req, res, next) {
